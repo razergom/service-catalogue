5c3383b4dab5ef120b121fd4fa12195c723f33dd
Andrey Petrov <thenolldd@gmail.com>
2021-04-22 14:43:29 +0300

    feat: get base64 file-content from github

--- routes/services.js
+++ routes/services.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const axios = require('axios')
 const gh = require('parse-github-url')
 const Service = require('../models/service')
 
@@ -16,9 +17,11 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
 	const parsedGithubUrl = gh(req.body.url)
 
-	const gitHubApiFileUrl = `https://api.github.com/repos/${parsedGithubUrl.repository}/contents/${parsedGithubUrl.filepath}`
+	const githubApiFileUrl = `https://api.github.com/repos/${parsedGithubUrl.repository}/contents/${parsedGithubUrl.filepath}`
 
-	res.json(gitHubApiFileUrl)
+	axios.get(githubApiFileUrl)
+		.then(response => res.json(Buffer.from(response.data.content, 'base64').toString()))
+		.catch(err => res.status(500).json({ message: err.message }))
 })
 
 module.exports = router
