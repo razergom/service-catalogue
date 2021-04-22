b2983333071e7b13cb74d03578c7c04e1be5dcec
Andrey Petrov <thenolldd@gmail.com>
2021-04-22 18:43:44 +0300

    feat: getting service by id

--- routes/services.js
+++ routes/services.js
@@ -53,6 +53,14 @@ router.post('/register', async (req, res) => {
 		.catch(err => res.status(404).json({ message: err.message }))
 })
 
+router.get('/:id', getService, async (req, res) => {
+	try {
+		res.status(200).json(res.service)
+	} catch (err) {
+		res.status(500).json({ message: err.message })
+	}
+})
+
 router.delete('/:id', getService, async (req, res) => {
 	try {
 		await res.service.remove()
